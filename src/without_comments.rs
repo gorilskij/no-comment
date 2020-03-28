880405449ca11ae4901bfa380c7027c1ecf3008e
gorilskij <43991400@users.noreply.github.com>
2020-03-28 10:36:59 +0100

    add documentation

--- src/without_comments.rs
+++ src/without_comments.rs
@@ -1,9 +1,17 @@
+/// `char` iterator that removes rust-style line (`// ...`) and block (`/* ... */`) comments.
+/// Nested block comments (`/* /* ... */ */`) are treated correctly.
+/// Unclosed block comments (`/* ...`) are closed automatically when the inner iterator finishes.
+/// Closing unopened block comments (`... */`) causes a panic.
 pub struct WithoutComments<I: Iterator<Item=char>> {
+    /// Inner `char` iterator
     iter: I,
+    /// A one-`char` buffer used to match 2-long constructs `//`, `/*`, and `*/`
     hold: Option<char>,
 }
 
-// called after a '/*' has been read, exhausts block comment including nested blocks
+/// Called after a `/*` has been read. Reads iterator until matching `*/` including nested block
+/// comments and dumps the output. The next item returned by the iterator would be the character
+/// straight after `*/` or `None` if `*/` occurs last or never occurs.
 fn exhaust_block_comment(iter: &mut impl Iterator<Item=char>) {
     let mut depth = 0_usize;
     let mut last_read = None;
@@ -38,12 +46,17 @@ fn exhaust_block_comment(iter: &mut impl Iterator<Item=char>) {
     }
 }
 
+/// Same as the `Option` type but with the additional `None`-like value `Wait` used to signify
+/// that an item cannot be returned at this time but that another attempt should be made (as
+/// opposed to `None` which means that the iteration has concluded).
 #[derive(Debug)]
 enum Tription<T> {
+    /// An item of type `T` is returned
     Some(T),
+    /// Iteration has completed
     None,
-    Wait, // this means that the iteration isn't done but
-          // one more call to next is required for the next element
+    /// No item can be returned but another attempt should be made
+    Wait,
 }
 
 impl<T> From<Option<T>> for Tription<T> {
@@ -56,6 +69,11 @@ impl<T> From<Option<T>> for Tription<T> {
 }
 
 impl<I: Iterator<Item=char>> WithoutComments<I> {
+    /// Inner equivalent of `Iterator::next` returning a `Tription` instead of an `Option`.
+    /// This is for the case where a block comment follows right after another
+    /// (`/* ... *//* ... */`), after reading `*/` and then `/`, the iterator can't yet return
+    /// an item because if the next character is `*`, another comment will be started, thus it
+    /// returns `Tription::Wait` instead.
     fn next_(&mut self) -> Tription<char> {
         if self.hold.is_none() {
             // return None if done
@@ -87,7 +105,7 @@ impl<I: Iterator<Item=char>> WithoutComments<I> {
                 Tription::Wait
             }
             ('*', '/') => {
-                panic!("*/ without /* (closed block comment at top level)")
+                panic!("Closed block comment at top level ('*/' without matching '/*')")
             }
             (h, n) => {
                 self.hold = Some(n);
@@ -111,11 +129,18 @@ impl<I: Iterator<Item=char>> Iterator for WithoutComments<I> {
     }
 }
 
+/// A trait to implement the `without_comments` method on all `Iterator<Item=char>`
 pub trait IntoWithoutComments where Self: Sized + Iterator<Item=char> {
-    fn without_comments(self) -> WithoutComments<Self>;
-}
-
-impl<I: Iterator<Item=char>> IntoWithoutComments for I {
+    /// Returns a `WithoutComments` iterator containing self
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use without_comments::IntoWithoutComments;
+    /// let with_comments = "S/*he */be/*lie*/ve//d";
+    /// let without_comments = with_comments.chars().without_comments().collect::<String>();
+    /// assert_eq!(&without_comments, "Sbeve");
+    /// ```
     fn without_comments(self) -> WithoutComments<Self> {
         WithoutComments {
             iter: self,
@@ -123,3 +148,6 @@ impl<I: Iterator<Item=char>> IntoWithoutComments for I {
         }
     }
 }
+
+/// Blanket implementation
+impl<I: Iterator<Item=char>> IntoWithoutComments for I {}
