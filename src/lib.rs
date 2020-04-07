8345196101f14c77f2c8e19f1f38a392121f7512
gorilskij <43991400@users.noreply.github.com>
2020-04-07 11:06:09 +0200

    remove TODO in last commit: fix REDME version number

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ extern crate derive_more;
 
 pub use without_comments::{IntoWithoutComments, WithoutComments};
 
-// TODO redocument, update readmes, mention that comments started in strings are still comments ("/*" starts a block comment)
-
 pub mod languages;
 mod without_comments;
 
