e4f878c8381d842611ad5d8c9ddbaeb31ac9bc65
gorilskij <43991400@users.noreply.github.com>
2026-05-21 05:45:03 +0200

    minor renaming and spelling fixes

--- src/without_comments.rs
+++ src/without_comments.rs
@@ -1,8 +1,9 @@
 use std::collections::VecDeque;
 
-/// Buffer type used in the `WithoutComments` iterator, `Deref`s to `VecDeque<char>`.
-/// The capacity of the inner `VecDeque<char>` is constant, it represents the maximum length of
-/// buffer needed to match any open or close pattern for the current language.
+/// Buffer type used in the `WithoutComments` iterator, `Deref`s to
+/// `VecDeque<char>`. The capacity of the inner `VecDeque<char>` is constant, it
+/// represents the maximum length of buffer needed to match any open or close
+/// pattern for the current language.
 #[derive(Deref, Debug)]
 #[repr(transparent)]
 struct Buf(VecDeque<char>);
@@ -16,8 +17,8 @@ impl Buf {
         self.len() == self.capacity()
     }
 
-    /// Fill up inner `VecDeque<char>` to capacity from provided iterator. This is the only way
-    /// to add elements to the buffer.
+    /// Fill up inner `VecDeque<char>` to capacity from provided iterator. This
+    /// is the only way to add elements to the buffer.
     fn fill_up(&mut self, iter: &mut impl Iterator<Item = char>) {
         while !self.is_full() {
             match iter.next() {
@@ -27,8 +28,8 @@ impl Buf {
         }
     }
 
-    /// Checks whether the beginning of the buffer matches the provided pattern, the buffer should
-    /// be full when this method is called.
+    /// Checks whether the beginning of the buffer matches the provided pattern,
+    /// the buffer should be full when this method is called.
     fn matches(&self, pat: &str) -> bool {
         self.iter().take(pat.len()).copied().eq(pat.chars())
     }
@@ -38,17 +39,19 @@ impl Buf {
         self.0.pop_front().unwrap()
     }
 
-    /// Assert that the buffer has at least n elements and pop the first n elements
+    /// Assert that the buffer has at least n elements and pop the first n
+    /// elements
     fn pop_front_n(&mut self, n: usize) {
         let _ = self.0.drain(..n);
     }
 }
 
-/// Same as the `Option` type but with the additional `None`-like value `Wait` used to signify
-/// that an item cannot be returned at this time but that another attempt should be made (as
-/// opposed to `None` which means that the iteration has concluded).
+/// Same as the `Option` type but with the additional `None`-like value `Wait`
+/// used to signify that an item cannot be returned at this time but that
+/// another attempt should be made (as opposed to `None` which means that the
+/// iteration has concluded).
 #[derive(Debug)]
-enum Tription<T> {
+enum Iteration<T> {
     /// An item of type `T` is returned
     Some(T),
     /// Iteration has completed
@@ -57,37 +60,40 @@ enum Tription<T> {
     Wait,
 }
 
-impl<T> From<Option<T>> for Tription<T> {
+impl<T> From<Option<T>> for Iteration<T> {
     fn from(o: Option<T>) -> Self {
         match o {
-            Some(t) => Tription::Some(t),
-            None => Tription::None,
+            Some(t) => Iteration::Some(t),
+            None => Iteration::None,
         }
     }
 }
 
-/// Represents a set of rules for matching a specific comment in a language, for example 'block
-/// comment in rust' or 'line comment in haskell'.
+/// Represents a set of rules for matching a specific comment in a language, for
+/// example 'block comment in rust' or 'line comment in haskell'.
 #[derive(Copy, Clone, Debug)]
 pub struct Comment {
     /// Open comment pattern, such as `/*`
     pub(crate) open_pat: &'static str,
     /// Close comment pattern, such as `*/`
     pub(crate) close_pat: &'static str,
-    /// Whether this type of comment can be nested. For example, rust block comments can be
-    /// nested while C block comments can't.
+    /// Whether this type of comment can be nested. For example, rust block
+    /// comments can be nested while C block comments can't.
     pub(crate) nests: bool,
-    /// Whether to return the close comment pattern. For example, in rust block comments `*/`
-    /// isn't returned while in rust line comments, `\n` is returned.
+    /// Whether to return the close comment pattern. For example, in rust block
+    /// comments `*/` isn't returned while in rust line comments, `\n` is
+    /// returned.
     pub(crate) keep_close_pat: bool, // whether to still return close_pat as part of the text
-    /// Whether to allow the close comment pattern in regular text. For example, in rust `*/`
-    /// will panic unless it closes a block comment while `\n` will be treated normally.
+    /// Whether to allow the close comment pattern in regular text. For example,
+    /// in rust `*/` will panic unless it closes a block comment while `\n`
+    /// will be treated normally.
     pub(crate) allow_close_pat: bool, // whether to allow close_pat without matching open_pat
 }
 
-/// `char` iterator that removes comments based on a list of `Comment` specifications.
-/// Unclosed comments (`//...` or `/*...` or equivalents) continue until the end of the iterator.
-/// Closing unopened block comments (`... */` or equivalent) causes a panic.
+/// `char` iterator that removes comments based on a list of `Comment`
+/// specifications. Unclosed comments (`//...` or `/*...` or equivalents)
+/// continue until the end of the iterator. Closing unopened block comments
+/// (`... */` or equivalent) causes a panic.
 pub struct WithoutComments<I: Iterator<Item = char>> {
     /// Inner `char` iterator
     iter: I,
@@ -95,11 +101,12 @@ pub struct WithoutComments<I: Iterator<Item = char>> {
     buf: Buf,
     /// List of types of comments and associated rules
     comments: Box<[Comment]>,
-    /// The current state. None represents normal text, i.e. not currently in a comment,
-    /// Some(idx, nesting) represents that the iterator is currently in a comment, idx
-    /// is the index of the current comment in self.comments, nesting is None if the current
-    /// comment doesn't nest and Some(d) otherwise, where d is the current nesting depth
-    /// starting at 0.
+    /// The current state. None represents normal text, i.e. not currently in a
+    /// comment, Some(idx, nesting) represents that the iterator is
+    /// currently in a comment, idx is the index of the current comment in
+    /// self.comments, nesting is None if the current comment doesn't nest
+    /// and Some(d) otherwise, where d is the current nesting depth starting
+    /// at 0.
     state: Option<(usize, Option<usize>)>,
 }
 
@@ -114,17 +121,19 @@ impl<I: Iterator<Item = char>> WithoutComments<I> {
         }
     }
 
-    /// Inner equivalent of `Iterator::next` returning a `Tription` instead of an `Option`.
-    /// This is for the case where a block comment follows right after another
-    /// (`/* ... *//* ... */` or equivalent), after reading `*/`, the buffer needs to be filled
-    /// to make sure that any eventual `/*` will be matched, this is done in the next call to
-    /// `next_`, thus, the calling loop in `Iterator::next` is told to wait one more iteration.
-    fn next_(&mut self) -> Tription<char> {
+    /// Inner equivalent of `Iterator::next` returning an `Iteration` instead of
+    /// an `Option`. This is for the case where a block comment follows
+    /// right after another (`/* ... *//* ... */` or equivalent), after
+    /// reading `*/`, the buffer needs to be filled to make sure that any
+    /// eventual `/*` will be matched, this is done in the next call to
+    /// `next_`, thus, the calling loop in `Iterator::next` is told to wait one
+    /// more iteration.
+    fn next_(&mut self) -> Iteration<char> {
         // at least one element missing from previous call
         self.buf.fill_up(&mut self.iter);
 
         if self.buf.is_empty() {
-            return Tription::None;
+            return Iteration::None;
         }
 
         // if in comment
@@ -137,7 +146,7 @@ impl<I: Iterator<Item = char>> WithoutComments<I> {
                 ..
             } = comment;
 
-            // check close before open to make thinks like python's '''...''' work
+            // check close before open to make things like python's '''...''' work
             if self.buf.matches(close_pat) {
                 // matched close pattern
 
@@ -163,7 +172,7 @@ impl<I: Iterator<Item = char>> WithoutComments<I> {
                 self.buf.pop_front();
             }
 
-            Tription::Wait
+            Iteration::Wait
         } else {
             // if in text
             // #![feature(bindings_after_at)]
@@ -187,14 +196,14 @@ impl<I: Iterator<Item = char>> WithoutComments<I> {
                         false => None,
                     };
                     self.state = Some((idx, nesting));
-                    return Tription::Wait;
+                    return Iteration::Wait;
                 } else if !allow_close_pat && self.buf.matches(close_pat) {
                     // if close pattern forbidden, panic
                     panic!("Got \"{}\" without matching \"{}\"", close_pat, open_pat)
                 }
             }
 
-            Tription::Some(self.buf.pop_front())
+            Iteration::Some(self.buf.pop_front())
         }
     }
 }
@@ -202,21 +211,23 @@ impl<I: Iterator<Item = char>> WithoutComments<I> {
 impl<I: Iterator<Item = char>> Iterator for WithoutComments<I> {
     type Item = char;
 
-    /// Simply calls `WithoutComments::next_`, a return value of `Tription::Wait` signifies
-    /// that another attempt should be made, `Tription::Some` and `Tription::None` are
-    /// equivalent to the same variants of the `Option` type.
+    /// Simply calls `WithoutComments::next_`, a return value of
+    /// `Iteration::Wait` signifies that another attempt should be made,
+    /// `Iteration::Some` and `Iteration::None` are equivalent to the same
+    /// variants of the `Option` type.
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             match self.next_() {
-                Tription::None => return None,
-                Tription::Some(c) => return Some(c),
-                Tription::Wait => (),
+                Iteration::None => return None,
+                Iteration::Some(c) => return Some(c),
+                Iteration::Wait => (),
             }
         }
     }
 }
 
-/// A trait to implement the `without_comments` method on all `Iterator<Item=char>`
+/// A trait to implement the `without_comments` method on all
+/// `Iterator<Item=char>`
 pub trait IntoWithoutComments
 where
     Self: Sized + Iterator<Item = char>,
@@ -225,13 +236,14 @@ where
     ///
     /// # Arguments
     ///
-    /// * `language` - A boxed slice containing all the comments that the returned iterator
+    /// * `language` - A boxed slice containing all the comments that the
+    ///   returned iterator
     /// will be removing
     ///
     /// # Example
     ///
     /// ```
-    /// use no_comment::{IntoWithoutComments, languages};
+    /// use no_comment::{languages, IntoWithoutComments};
     /// let with_comments = "S/*he */be/*lie*/ve//d";
     /// let without_comments = with_comments
     ///     .chars()
@@ -242,9 +254,7 @@ where
     fn without_comments(self, language: Box<[Comment]>) -> WithoutComments<Self> {
         let mut buf_len = 0;
         for &Comment {
-            open_pat,
-            close_pat,
-            ..
+            open_pat, close_pat, ..
         } in language.iter()
         {
             if open_pat.len() > buf_len {
